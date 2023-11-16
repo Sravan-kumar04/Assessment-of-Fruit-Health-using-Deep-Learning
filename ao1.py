eac349558c31c46a59a124265e39885eafc4e8a2
Sravan-kumar04 <tiprisshravan@gmail.com>
2023-11-16 16:11:53 +0530

    required files

---
+++ ao1.py
@@ -0,0 +1,72 @@
+from flask import Flask,render_template,request
+from keras.models import load_model
+import tensorflow
+
+import numpy as np
+from keras.models import Sequential
+
+from keras.preprocessing import image
+
+
+
+app=Flask(__name__)
+
+labels=['freshApple','freshBanana','freshOrange','rottenApple','rottenBanana','rottenOrange']
+model=load_model("Fresh_Rotten_Fruits_MobileNetV2_Transfer_Learning2(98).h5")
+model.make_predict_function()
+
+def predict_label(img_path):
+    i=tensorflow.keras.utils.load_img(img_path,target_size=(224,224))
+    i=np.array(i)/255.0
+    i=i.reshape(1,224,224,3)
+    predictions = model.predict(i)  
+    index=predictions.argmax()
+    print(index)
+    print(predictions)
+    
+   
+    return np.max(predictions);
+
+def predfruit(img_path):
+    i=tensorflow.keras.utils.load_img(img_path,target_size=(224,224))
+    i=np.array(i)/255.0
+    i=i.reshape(1,224,224,3)
+    predictions = model.predict(i)
+    index=predictions.argmax()
+
+    if labels[index].startswith('f'):
+        return 'fresh'
+    else:
+        return 'rotten'
+
+def dfruit(img_path):
+    i=tensorflow.keras.utils.load_img(img_path,target_size=(224,224))
+    i=np.array(i)/255.0
+    i=i.reshape(1,224,224,3)
+    predictions = model.predict(i)  
+    index=predictions.argmax()
+    if labels[index].startswith('f'):
+        return labels[index][5:]
+    else:
+        return labels[index][6:]
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+@app.route("/submit",methods=['GET',"POST"])
+def get_output():
+    if request.method=="POST":
+        img=request.files['my_image']
+        try:
+            img_path="static/"+img.filename
+            img.save(img_path)
+            p=predict_label(img_path) 
+            k=predfruit(img_path)
+            return render_template("ps.html",prediction=k,result=round(p*100,3),ness=k,fruit=dfruit(img_path),img_path=img_path)
+        except:
+            return render_template("ps.html")
+    return render_template("ps.html")
+if __name__=="__main__":
+    app.  run(debug=True)
+
+
